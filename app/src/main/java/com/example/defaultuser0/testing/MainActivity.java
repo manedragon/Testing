d25fac32d680691c7fe5068109049f0fe5544ffd
manedragonbird <manuel26live@gmail.com>
2016-10-31 18:30:27 -0600

    Initial commit

---
+++ app/src/main/java/com/example/defaultuser0/testing/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.defaultuser0.testing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView texto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        texto = (TextView) findViewById(R.id.texto);
+        texto.setText("jojoj");
+    }
+}
